f84c8df4609472c835e89812c309c7b48403d386
KordianD <KordianD@users.noreply.github.com>
2019-11-16 12:41:48 +0100

    Added another strategy

--- strategies.py
+++ strategies.py
@@ -2,12 +2,20 @@ from config import BETRAYAL, SILENCE
 
 
 class AlwaysBetrayal:
-    def answer(self) -> int:
+    def answer(self, answer_of_competitor) -> int:
         return BETRAYAL
 
 
 class AlwaysSilence:
-    def answer(self) -> int:
+    def answer(self, answer_of_competitor) -> int:
         return SILENCE
 
 
+class PreviousCompetitorAnswer:
+    def __init__(self):
+        self.previous_competitor_answer = SILENCE
+
+    def answer(self, answer_of_competitor):
+        actual_answer = self.previous_competitor_answer
+        self.previous_competitor_answer = answer_of_competitor
+        return actual_answer
