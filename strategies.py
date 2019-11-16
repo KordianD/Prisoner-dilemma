99395f64e65ef729418bd671d7ee281c7ed02224
KordianD <KordianD@users.noreply.github.com>
2019-11-16 12:55:18 +0100

    Added Alternate strategy

--- strategies.py
+++ strategies.py
@@ -2,20 +2,43 @@ from config import BETRAYAL, SILENCE
 
 
 class AlwaysBetrayal:
-    def answer(self, answer_of_competitor) -> int:
+    def answer(self) -> int:
         return BETRAYAL
+
+    def set_competitor_answer(self):
+        pass
 
 
 class AlwaysSilence:
-    def answer(self, answer_of_competitor) -> int:
+    def answer(self) -> int:
         return SILENCE
+
+    def set_competitor_answer(self):
+        pass
 
 
 class PreviousCompetitorAnswer:
     def __init__(self):
         self.previous_competitor_answer = SILENCE
 
-    def answer(self, answer_of_competitor):
-        actual_answer = self.previous_competitor_answer
+    def answer(self):
+        return self.previous_competitor_answer
+
+    def set_competitor_answer(self, answer_of_competitor):
         self.previous_competitor_answer = answer_of_competitor
-        return actual_answer
+
+
+class AlternatelyAnswer:
+    def _init__(self):
+        self.previous_self_answer = SILENCE
+
+    def answer(self):
+        if self.previous_self_answer == SILENCE:
+            self.previous_self_answer = BETRAYAL
+        else:
+            self.previous_self_answer = SILENCE
+
+        return self.previous_self_answer
+
+    def set_competitor_answer(self):
+        pass
