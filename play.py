562897c1b54835f8301d04694bf48967ba523bb3
KordianD <KordianD@users.noreply.github.com>
2019-11-16 13:16:02 +0100

    Resolve hiding name problem

--- play.py
+++ play.py
@@ -3,7 +3,7 @@ from config import ALL_STRATEGIES
 print("Start ")
 
 for strategy in ALL_STRATEGIES:
-    for strategy in ALL_STRATEGIES:
+    for inner_strategy in ALL_STRATEGIES:
         pass
 
 print("Stop ")
