904d017acfcedbd8b0550094d6297034556a1daf
dan-gut <danielgut95@gmail.com>
2019-11-17 21:43:46 +0100

    Added RandomChoice Randomized init of PreviousCompetitorAnswer and AlternatelyAnswer

--- strategies.py
+++ strategies.py
@@ -1,4 +1,5 @@
 from config import BETRAYAL, SILENCE
+import random
 
 
 class AlwaysBetrayal:
@@ -19,7 +20,7 @@ class AlwaysSilence:
 
 class PreviousCompetitorAnswer:
     def __init__(self):
-        self.previous_competitor_answer = SILENCE
+        self.previous_competitor_answer = random.choice([SILENCE, BETRAYAL])
 
     def answer(self):
         return self.previous_competitor_answer
@@ -30,7 +31,7 @@ class PreviousCompetitorAnswer:
 
 class AlternatelyAnswer:
     def __init__(self):
-        self.previous_self_answer = SILENCE
+        self.previous_self_answer = random.choice([SILENCE, BETRAYAL])
 
     def answer(self):
         if self.previous_self_answer == SILENCE:
@@ -39,6 +40,14 @@ class AlternatelyAnswer:
             self.previous_self_answer = SILENCE
 
         return self.previous_self_answer
+
+    def set_competitor_answer(self, answer_of_competitor: int):
+        pass
+
+
+class RandomChoice:
+    def answer(self) -> int:
+        return random.choice([SILENCE, BETRAYAL])
 
     def set_competitor_answer(self, answer_of_competitor: int):
         pass
--- strategy_factory.py
+++ strategy_factory.py
@@ -1,5 +1,5 @@
-from strategies import AlwaysBetrayal, AlwaysSilence, PreviousCompetitorAnswer, AlternatelyAnswer
-from config import ALTERNATELY_ANSWER, ALWAYS_SILENCE, ALWAYS_BETRAYAL, PREVIOUS_COMPETITOR_ANSWER
+from strategies import AlwaysBetrayal, AlwaysSilence, PreviousCompetitorAnswer, AlternatelyAnswer, RandomChoice
+from config import ALTERNATELY_ANSWER, ALWAYS_SILENCE, ALWAYS_BETRAYAL, PREVIOUS_COMPETITOR_ANSWER, RANDOM_CHOICE
 
 
 def create_strategy(name: str):
@@ -11,3 +11,5 @@ def create_strategy(name: str):
         return PreviousCompetitorAnswer()
     elif name == ALTERNATELY_ANSWER:
         return AlternatelyAnswer()
+    elif name == RANDOM_CHOICE:
+        return RandomChoice()
